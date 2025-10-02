f1b763bf2a9acef4ebe27827714d4161dcb3b3da
Claudio9905 <cloudpost10@gmail.com>
2025-10-02 17:53:33 +0200

    Update File

--- epi_atac/src/main/java/claudiopostiglione/dao/TrattaDAO.java
+++ epi_atac/src/main/java/claudiopostiglione/dao/TrattaDAO.java
@@ -1,8 +1,12 @@
 package claudiopostiglione.dao;
 
+import claudiopostiglione.entities.PuntoEmissione;
 import claudiopostiglione.entities.Tratta;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class TrattaDAO {
     private final EntityManager em;
@@ -18,5 +22,22 @@ public class TrattaDAO {
         em.persist(tratta);
         transaction.commit();
         System.out.println("Tratta salvata! :)");
+    }
+
+    public List<Tratta> findAllTratta() {
+        try {
+            TypedQuery<Tratta> query = em.createQuery("SELECT pe FROM Tratta pe", Tratta.class);
+            List<Tratta> found = query.getResultList();
+
+            // Stampa ogni UUID su una nuova riga
+//            for (Tratta id : found) {
+//                System.out.println(id);
+//            }
+
+            return found;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
     }
 }
